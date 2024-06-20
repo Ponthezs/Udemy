1e5dc5c890a4afe243a30de2f78e2bc5fd7c37ff
Felipe Pontes <pontesfelipe835@gmail.com>
2024-06-20 19:57:17 -0300

    Feat: new
    new

---
+++ 10-Primeiro Programa em C#/ProjectOne/ProjectOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+namespace ProjectOne // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
