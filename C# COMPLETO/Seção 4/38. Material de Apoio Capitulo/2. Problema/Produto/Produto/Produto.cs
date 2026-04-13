e21abe652e2c2266a4e547d759c1866bd5a92d27
Felipe Pontes <pontesfelipe835@gmail.com>
2026-04-13 15:25:17 -0300

    Udemy Secçao 4

---
+++ C# COMPLETO/Seção 4/38. Material de Apoio Capitulo/2. Problema/Produto/Produto/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Produto
+{
+    public string Nome;
+    public double Preco;
+    public int Quantidade;
+
+    public double ValorTotalEmEstoque()
+    {
+        return Preco * Quantidade;
+    }
+
+    public void AdicionarProdutos(int quantidade)
+    {
+        Quantidade += quantidade;
+    }
+
+    public void RemoverProdutos(int quantidade)
+    {
+        Quantidade -= quantidade;
+    }
+
+    public override string ToString()
+    {
+        return Nome
+            + ", $ "
+            + Preco.ToString("F2")
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2");
+    }
+}
---
+++ C# COMPLETO/Seção 4/38. Material de Apoio Capitulo/2. Problema/Produto/Produto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto produto = new Produto();
+
+        Console.WriteLine("Entre os dados do produto:");
+        Console.Write("Nome: ");
+        produto.Nome = Console.ReadLine();
+
+        Console.Write("Preço: ");
+        produto.Preco = double.Parse(Console.ReadLine());
+
+        Console.Write("Quantidade no estoque: ");
+        produto.Quantidade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nDados do produto: " + produto);
+
+        Console.Write("\nDigite o número de produtos a ser adicionado ao estoque: ");
+        int qtd = int.Parse(Console.ReadLine());
+        produto.AdicionarProdutos(qtd);
+
+        Console.WriteLine("Dados atualizados: " + produto);
+
+        Console.Write("\nDigite o número de produtos a ser removido do estoque: ");
+        qtd = int.Parse(Console.ReadLine());
+        produto.RemoverProdutos(qtd);
+
+        Console.WriteLine("Dados atualizados: " + produto);
+    }
+}
